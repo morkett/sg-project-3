a089ce59feac0438dc3549a6fd3da4b882b4850c
morkett <dcorkett@spartaglobal.co>
2017-04-10 23:35:05 +0100

    added error messages to api movie controller

--- api/controllers/movie.js
+++ api/controllers/movie.js
@@ -25,9 +25,9 @@ function showMovieSearch(req, res) {
 }
 
 
-    ///////////////////////////
+             ///////////////////////////
 //////////// getOne movie, video, similar ////////////
-    ///////////////////////////
+              ///////////////////////////
 function getOne(req,res) {
   var movieId = req.params.movieId;
 
@@ -42,25 +42,25 @@ function getOne(req,res) {
     var similarMoviesResultsJson;
 
     singleMovieResultsJson = JSON.parse(body);
-    // if (error) {
-    //   console.warn('Could not get movie:', error);
-    //   res.status(500).json({ message: 'Could not get one movie - please check server logs' });
-    //   return;
-    // }
+    if (error) {
+      console.warn('Could not get movie:', error);
+      res.status(500).json({ message: 'Could not get one movie - please check server logs' });
+      return;
+    }
     request(urlVids, (error,response,body) => {
       singleVideoResultsJson = JSON.parse(body);
-      // if (error) {
-      //   console.warn('Could not get video:', error);
-      //   res.status(500).json({ message: 'Could not get one video - please check server logs' });
-      //   return;
-      // }
+      if (error) {
+        console.warn('Could not get video:', error);
+        res.status(500).json({ message: 'Could not get one video - please check server logs' });
+        return;
+      }
       request(urlSimilar, (error,response,body) => {
 
-        // if (error) {
-        //   console.warn('Could not get video:', error);
-        //   res.status(500).json({ message: 'Could not get one video - please check server logs' });
-        //   return;
-        // }
+        if (error) {
+          console.warn('Could not get video:', error);
+          res.status(500).json({ message: 'Could not get one similar - please check server logs' });
+          return;
+        }
         similarMoviesResultsJson = JSON.parse(body);
         console.log('VIDEO',singleVideoResultsJson);
         var responseJson = {
@@ -83,7 +83,6 @@ function getMainList(req,res) {
 
   var urlInTheatres = `${tmdb.TMDB_BASE_URL}/discover/movie?primary_release_date.gte=2017-04-11&primary_release_date.lte=2017-05-30?&api_key=${tmdb.TMDB_API_KEY}&sort_by=popularity.desc&language=en-US`;
   var urlPopular = `${tmdb.TMDB_BASE_URL}/discover/movie?sort_by=popularity.asc?&api_key=${tmdb.TMDB_API_KEY}&sort_by=popularity.desc&language=en-US`;
-  // var urlKids = `${tmdb.TMDB_BASE_URL}/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc?&api_key=${tmdb.TMDB_API_KEY}&language=en-US`;
   var urlSciFi = `${tmdb.TMDB_BASE_URL}/discover/movie?with_genres=${tmdb.GENRE_SCIFI}&sort_by=vote_average.desc?&api_key=${tmdb.TMDB_API_KEY}&sort_by=popularity.desc&language=en-US`;
   var urlRomance = `${tmdb.TMDB_BASE_URL}/discover/movie?with_genres=${tmdb.GENRE_ROMANCE}&sort_by=vote_average.desc?&api_key=${tmdb.TMDB_API_KEY}&sort_by=popularity.desc&language=en-US`;
   var urlHorror = `${tmdb.TMDB_BASE_URL}/discover/movie?with_genres=${tmdb.GENRE_HORROR}&sort_by=vote_average.desc?&api_key=${tmdb.TMDB_API_KEY}&sort_by=popularity.desc&language=en-US`;
@@ -94,7 +93,6 @@ function getMainList(req,res) {
   request(urlInTheatres, (error,response,body) => {
     var inTheatresResultsJson;
     var popularResultsJson;
-    // var kidsResultsJson;
     var sciFiResultsJson;
     var romanceResultsJson;
     var horrorResultsJson;
@@ -103,39 +101,66 @@ function getMainList(req,res) {
     var actionResultsJson;
 
     inTheatresResultsJson = JSON.parse(body);
-    // if (error) {
-    //   console.warn('Could not get movie:', error);
-    //   res.status(500).json({ message: 'Could not get one movie - please check server logs' });
-    //   return;
-    // }
+    if (error) {
+      console.warn('Could not get movie:', error);
+      res.status(500).json({ message: 'Could not get inTheatresResultsJson - please check server logs' });
+      return;
+    }
     request(urlPopular, (error,response,body) => {
       popularResultsJson = JSON.parse(body);
-      // if (error) {
-      //   console.warn('Could not get video:', error);
-      //   res.status(500).json({ message: 'Could not get one video - please check server logs' });
-      //   return;
-      // }
-      // request(urlKids, (error,response,body) => {
-      //   kidsResultsJson = JSON.parse(body);
+      if (error) {
+        console.warn('Could not get video:', error);
+        res.status(500).json({ message: 'Could not get popularResultsJson - please check server logs' });
+        return;
+      }
+
 
       request(urlSciFi, (error,response,body) => {
         sciFiResultsJson = JSON.parse(body);
-
+        if (error) {
+          console.warn('Could not get video:', error);
+          res.status(500).json({ message: 'Could not get sciFiResultsJson - please check server logs' });
+          return;
+        }
         request(urlRomance, (error,response,body) => {
           romanceResultsJson = JSON.parse(body);
-
+          if (error) {
+            console.warn('Could not get video:', error);
+            res.status(500).json({ message: 'Could not get romanceResultsJson - please check server logs' });
+            return;
+          }
 
           request(urlHorror, (error,response,body) => {
             horrorResultsJson = JSON.parse(body);
+            if (error) {
+              console.warn('Could not get video:', error);
+              res.status(500).json({ message: 'Could not get horrorResultsJson - please check server logs' });
+              return;
+            }
 
             request(urlComedy, (error,response,body) => {
               comedyResultsJson = JSON.parse(body);
+              if (error) {
+                console.warn('Could not get video:', error);
+                res.status(500).json({ message: 'Could not get comedyResultsJson - please check server logs' });
+                return;
+              }
 
               request(urlAni, (error,response,body) => {
                 animationResultsJson = JSON.parse(body);
+                if (error) {
+                  console.warn('Could not get video:', error);
+                  res.status(500).json({ message: 'Could not get animationResultsJson - please check server logs' });
+                  return;
+                }
 
                 request(urlAction, (error,response,body) => {
                   actionResultsJson = JSON.parse(body);
+                  if (error) {
+                    console.warn('Could not get video:', error);
+                    res.status(500).json({ message: 'Could not get actionResultsJson - please check server logs' });
+                    return;
+                  }
 
                   var responseJson = {
                     inTheatresResultsJson,
@@ -149,10 +174,7 @@ function getMainList(req,res) {
                     actionResultsJson
                   };
                   res.json(responseJson);
-
-                  console.log('ResultsJson:',responseJson);
                 });
-
               });
             });
           });
@@ -160,13 +182,7 @@ function getMainList(req,res) {
       });
     });
   });
-  // });
-// JSON.parse convertsstring into JSON
-
-
-
 }
-
 
 
 module.exports = {
